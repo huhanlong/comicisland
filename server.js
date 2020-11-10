5be3e18f281dfd42e74561d7624aa4767bf82d01
huhanlong <1836597145@qq.com>
2020-11-10 20:25:42 +0800

    '脚手架配置代理'

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+const app = express();
+
+//处理一下跨源
+app.use(cors())
+
+// 目标地址 C 是: https://api.asilu.com/today/
+// 这是我们可以访问 B 是：http://localhost:9090/today/
+
+// A 发起请求 http://localhost:9090/migu/today/
+
+//提供代理，处理跨源的数据
+app.use('/migu', createProxyMiddleware({
+  // 目标地址，只写主机
+  target: 'https://api.asilu.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/migu': ''
+  }
+}))
+
+app.get('/', (req, res) => {
+  res.send("hello world")
+})
+
+
+app.listen(9090, () => {
+  console.log("服务器启动成功");
+})
+
+//运行这个程序 npm run start  或者 npm start
+//supervisor server.js
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      // key: value
+      //  key - 前缀  /migu
+      // value - 跟 http-proxy-middleware 相同的一个配置
+      '/migu': {
+        target: 'https://api.asilu.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/migu': ''
+        }
+      },
+      '/api': {
+        target: 'https://mhd.zhuishushenqi.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
